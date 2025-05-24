30a2a950b6b00f0a9e55f1912ee4bbc6e5e9e8ed
Daniella Marbell <zms510@york.ac.uk>
2025-05-24 17:29:12 +0100

    create a dataframe with a Sea level column without the correct data values

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -24,27 +24,50 @@ def read_tidal_data(filename):
     date_object = datetime.datetime.strptime(date_string, "%Y-%m-%d-%H")
     print(date_object)
         
-#make dataframe    
-    string = "sea_level"                                       #
-    dates = pd.date_range("19470101", periods=8760, freq='h')
+    
+    # create datetime index
+    dates = pd.date_range("19470101", periods=8760, freq='H')
     dates
-    df = pd.DataFrame(np.random.randn(8760, 1), index=dates, columns=["Sea_level"])
+    df = pd.DataFrame(np.random.randn(8760,1), index=dates, columns=["Sea_level"])
     df
-    return 0
+    
+
+
+
+
+
+
+    
+    # Create 8760 hourly timestamps using datetime and a loop
+    start = datetime.datetime(1947, 1, 1, 0)
+    datetimes = [start + datetime.timedelta(hours=i) for i in range(8760)]
+
+    # Convert those to your custom string format: "YYYY-M-D-H"
+    date_strings = [dt.strftime("%Y-%m-%d-%H") for dt in datetimes]  
+
+    # Now parse them back using strptime
+    parsed_dates = [datetime.datetime.strptime(ds, "%Y-%m-%d-%H") for ds in date_strings]
+
+    # Print first 5 to verify
+    for d in parsed_dates[:5]:
+        print(d)
+
     
 def extract_single_year_remove_mean(year, data):
-   
+
 
     return 
 
 
 def extract_section_remove_mean(start, end, data):
+  
 
 
     return 
 
 
 def join_data(data1, data2):
+ 
 
     return 
 
@@ -52,15 +75,17 @@ def join_data(data1, data2):
 
 def sea_level_rise(data):
 
+
                                                      
     return 
 
 def tidal_analysis(data, constituents, start_datetime):
-
+ 
 
     return 
 
 def get_longest_contiguous_data(data):
+ 
 
 
     return 
